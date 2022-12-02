25be664972bfdb5abf52899bb3f40001730b9d94
Michael Knepprath <mknepprath@gmail.com>
2022-12-02 16:12:29 -0500

    Add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Michael Knepprath</h1>
+
+      <p>
+        How about I just copy the information from the about page on my website?
+        Here you go:
+      </p>
+
+      <p>
+        Michael Knepprath is a Staff Software Engineer at{" "}
+        <a href="https://www.walmartlabs.com">Walmart</a> (prev.{" "}
+        <a href="https://hyper.online">Hyper</a>,{" "}
+        <a href="https://whcc.com">WHCC</a>). He loves his family and working on
+        side projects related to technology, design, film, video games, and so
+        on.
+      </p>
+    </div>
+  );
+}
