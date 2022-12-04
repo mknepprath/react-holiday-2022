6d28caea7e094491c451db06ebbcc44555a15d08
Michael Knepprath <mknepprath@gmail.com>
2022-12-04 17:52:40 -0500

    Added about/head

---
+++ app/about/head.tsx
@@ -0,0 +1,11 @@
+export default function AboutHead() {
+  return (
+    <>
+      <title>About Michael Knepprath</title>
+      <meta
+        name="description"
+        content="Michael Knepprath is a doing a thing."
+      />
+    </>
+  );
+}
