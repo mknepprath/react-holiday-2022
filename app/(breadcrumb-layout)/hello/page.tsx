5cf74bf45532fd1abda5277ccc6dcc50584b6228
Michael Knepprath <mknepprath@gmail.com>
2022-12-09 19:56:32 -0500

    Added loading state to /hello.

--- app/(breadcrumb-layout)/hello/page.tsx
+++ app/(breadcrumb-layout)/hello/page.tsx
@@ -2,6 +2,9 @@ export default async function HelloPage() {
   let req = await fetch("http://localhost:3000/api/hello");
   let res = await req.json();
 
+  // proc the loading state
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   return (
     <div>
       <h1>Hello {res.name}</h1>
