2f89675e213d6958640655f96949891c6307fcf1
Michael Knepprath <mknepprath@gmail.com>
2022-12-11 17:23:54 -0500

    Fixed /hello PageProps.

--- app/(breadcrumb-layout)/hello/page.tsx
+++ app/(breadcrumb-layout)/hello/page.tsx
@@ -1,10 +1,10 @@
-export default async function HelloPage({
-  searchParams,
-}: {
-  searchParams: { code?: string };
-}) {
+interface PageProps {
+  searchParams?: { code: string };
+}
+
+export default async function HelloPage({ searchParams }: PageProps) {
   // let res = await fetch(`${process.env.BASE_URL}/api/hello`);
-  let res = await fetch(`https://httpstat.us/${searchParams.code || 500}`);
+  let res = await fetch(`https://httpstat.us/${searchParams?.code || 500}`);
 
   // fake the loading state
   await new Promise((resolve) => setTimeout(resolve, 1000));
